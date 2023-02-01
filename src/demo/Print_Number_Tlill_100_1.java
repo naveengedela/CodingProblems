21c889651572a120ba4254b20f46ec66bad4745a
naveeng <48471144+naveengedela@users.noreply.github.com>
2023-02-01 21:50:48 +0530

    Print till 💯

---
+++ src/demo/Print_Number_Tlill_100_1.java
@@ -0,0 +1,15 @@
+package problems;
+
+public class Print_Number_Tlill_100_1 {
+
+	public static void main(String[] args) {
+		int one = 'a' / 'a';
+		int last = "**********".length();
+		String ch = "100";
+ 		
+		for (int i= one; i<='d';i++) {
+			System.out.println(i);
+		}
+	}
+
+}
